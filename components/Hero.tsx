d63440784c991e13e640808ed170697169ceb92f
Ikoyo Daniel Omovigho <dannirolands@gmail.com>
2022-12-31 18:44:16 +0100

    fixed linting issues

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,5 +1,6 @@
 import Image from "next/image";
 import React from "react";
+
 import hero from "../public/images/HeroImagelg.png";
 import LargeDeviceSearch from "./LargeDeviceSearch";
 import MobileSearch from "./MobileSearch";
